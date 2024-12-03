4b3086cba15ddac91cc4ae3cbd178cf7dd763013
Flaxx1 <tummare@web.de>
2024-12-03 13:58:58 +0100

    Full build

--- RageQuit.bat
+++
@@ -1,1 +0,0 @@
-C:\PATH_TO_YOUR_PYTHON\python.exe C:\PATH_TO_RageQuitter.py\RageQuitter.py
--- RageQuitter.py
+++ RageQuitter.py
@@ -1,17 +1,25 @@
 import os
 import psutil
+import json
+
+# Lade die Konfiguration aus einer JSON-Datei
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        return json.load(file)
 
 def stop_processes_in_folder(folder_paths, exclude_process_names):
     for folder_path in folder_paths:
-       for proc in psutil.process_iter(['pid', 'name', 'exe']):
+        for proc in psutil.process_iter(['pid', 'name', 'exe']):
             if proc.info['exe'] is None:
                 continue
             if any(folder_path in proc.info['exe'] for folder_path in folder_paths) and proc.info['name'] not in exclude_process_names:
                 proc.kill()
 
-## Paths where the Games.exe is located
-folder_paths = [r'D:\Steam\steamapps', r'D:\Epic Games'] 
-## Which .exe should be ignored
-exclude_process_names = ['wallpaperservice32_c.exe', 'wallpaper32.exe', 'EpicGamesLauncher.exe', 'EpicWebHelper.exe']
+# Konfigurationsdatei laden
+config = load_config("config.json")
+
+# Daten aus der Konfiguration verwenden
+folder_paths = config["folder_paths"]
+exclude_process_names = config["exclude_process_names"]
 
 stop_processes_in_folder(folder_paths, exclude_process_names)
